79097a5437e70076a83619c11ca4ed39d7895ba5
unknown <malikolaide081@gmail.com>
2023-07-08 11:06:06 -0700

    day 02

---
+++ 02_class/02_work/01_list.dart
@@ -0,0 +1,15 @@
+// list
+
+
+void main(List<String> argument) {
+  List<int> mylist = [1,2,3,4,];
+  final firstElement = mylist[0];
+
+  final myList2 = [1,2,3];
+  
+  <int>[1,2,3];
+   
+   print(mylist);
+
+
+}
