a05d0e7614d4f1bfdf8955e19184df9e7fb50b68
unknown <malikolaide081@gmail.com>
2023-07-19 05:25:56 -0700

    assessment

---
+++ 03_class/14.list.dart
@@ -0,0 +1,12 @@
+void main() {
+
+ dynamic dynamicvariable = "A boy";
+ print(dynamicvariable);
+
+ dynamicvariable = 15;
+ print(dynamicvariable);
+
+ dynamicvariable = true;
+ print(dynamicvariable);
+
+}
--- 03_class/l4_list.dart
+++
@@ -1,9 +0,0 @@
-void main() {
-
-//find all german names
-List<Friend> myFriend = [
-  Friend(name: 'Mikel', originCountry: 'Nigeria'),
-  Friend(name: 'David', originCountry: 'France')
-]
-
-}
