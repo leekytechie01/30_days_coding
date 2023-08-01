5e85aaaab277abbfc73468e63b0439029abe276e
unknown <malikolaide081@gmail.com>
2023-08-01 11:26:04 -0700

    tue/01

--- 01.lettextcode.dart
+++
@@ -1,5 +0,0 @@
-import 'dart:html';
-
-void main() {
-  Let text1 ="log(text3);
-}
---
+++ 01_temp.dart
@@ -0,0 +1,6 @@
+void main (){
+  //conversion of temperature
+  var fahrenheit = 50;
+  var celsius = (fahrenheit - 32) * 5/9;
+  print(celsius);
+}
