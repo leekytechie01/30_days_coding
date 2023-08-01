4c0cfc3eaf930d9119d00fb73b30e02fda32d631
unknown <malikolaide081@gmail.com>
2023-08-01 11:48:04 -0700

    tue/01

--- 01_temp.dart
+++ 01_temp.dart
@@ -1,6 +1,9 @@
 void main (){
   //conversion of temperature
-  var fahrenheit = 50;
+  var F = 50;
+  var C = 40;
+  var fahrenheit = 42;
   var celsius = (fahrenheit - 32) * 5/9;
   print(celsius);
+  print(fahrenheit + C);
 }
